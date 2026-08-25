756e5a72fda318e5f61616bf425b118061aabb49
Itiel Beeri <itiel.beeri@gmail.com>
2026-08-25 13:43:10 +0300

    recommendations

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface Window {
+  __lenis?: {
+    stop: () => void;
+    start: () => void;
+    scrollTo: (target: any, options?: any) => void;
+    [key: string]: any;
+  };
+}
